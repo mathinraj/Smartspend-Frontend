89100c4fa12c8c84c2c74fdd39df5c79fc4ce070
Mathinraj <mathinraj7@gmail.com>
2025-01-20 00:01:42 +0530

    Added favicon and background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,59 @@
+// import React from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+
+// const Header = () => {
+//     const navigate = useNavigate();
+//     const username = localStorage.getItem('username');
+
+//     const handleLogout = () => {
+//         localStorage.removeItem('role');
+//         localStorage.removeItem('username');
+//         navigate('/login');
+//     };
+
+//     return (
+//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//             <div className="container-fluid">
+//                 <Link className="navbar-brand" to="/">
+//                     Expense Tracker
+//                 </Link>
+//                 <div className="collapse navbar-collapse" id="navbarNav">
+//                     <ul className="navbar-nav me-auto">
+//                         <li className="nav-item">
+//                             <Link className="nav-link" to="/">
+//                                 Home
+//                             </Link>
+//                         </li>
+//                         {username && (
+//                             <li className="nav-item">
+//                                 <Link className="nav-link" to="/dashboard">
+//                                     Dashboard
+//                                 </Link>
+//                             </li>
+//                         )}
+//                     </ul>
+//                     {username && (
+//                         <div className="d-flex">
+//                             <span className="navbar-text me-3">Welcome, {username}!</span>
+//                             <button className="btn btn-outline-danger" onClick={handleLogout}>
+//                                 Logout <i class="fa-solid fa-arrow-right-from-bracket"></i>
+//                             </button>
+//                         </div>
+//                     )}
+//                 </div>
+//             </div>
+//         </nav>
+//     );
+// };
+
+// export default Header;
+
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../assets/Images/Logo_Smartspend.svg';
+import "../styles/global.css"
 
-const Header = () => {
+const Header = ({ className }) => {
     const navigate = useNavigate();
     const username = localStorage.getItem('username');
 
@@ -12,10 +64,14 @@ const Header = () => {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className={`navbar navbar-expand-lg navbar-light ${className || 'bg-light'}`}>
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
-                    Expense Tracker
+                <img
+                        src={Logo}
+                        alt="Smart Spend Logo"
+                        style={{ height: '40px' }} // Adjust height as needed
+                    />
                 </Link>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav me-auto">
@@ -36,7 +92,7 @@ const Header = () => {
                         <div className="d-flex">
                             <span className="navbar-text me-3">Welcome, {username}!</span>
                             <button className="btn btn-outline-danger" onClick={handleLogout}>
-                                Logout <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                                Logout
                             </button>
                         </div>
                     )}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../styles/global.css"
 
 const HomePage = () => {
     return (
-        <div className="container">
+        <div className="container home-page-background">
             <div className="d-flex align-items-center justify-content-center vh-100">
                 <div className="text-center">
                     <h1>Welcome to SmartSpend !</h1>
                     <p>Manage your expenses efficiently.</p>
-                    <Link to="/login" className="btn btn-success">Login <i class="fa-solid fa-right-to-bracket"></i></Link>
+                    <Link to="/login" className="btn btn-success">Login <i className="fa-solid fa-right-to-bracket"></i></Link>
                 </div>
             </div>
         </div>
